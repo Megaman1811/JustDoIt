30cfdfb573a80221cef0e9fbaafda684a811cda5
Cristian Cerroni <cristian.cerroni@gmail.com>
2018-11-30 15:37:18 -0500

    Rename to ManageTask

--- app/src/main/java/ca/cerroni/justdoit/ManageTask.java
+++ app/src/main/java/ca/cerroni/justdoit/ManageTask.java
@@ -7,16 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class addTask extends AppCompatActivity {
+public class ManageTask extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_task);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_manage_task);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
