d1bad07394da7a483cdfc4e88a5512d1822a024a
Cristian Cerroni <cristian.cerroni@gmail.com>
2018-11-30 16:26:09 -0500

    Added functionality to switch to Manage Task view in menu option

--- app/src/main/java/ca/cerroni/justdoit/MainActivity.java
+++ app/src/main/java/ca/cerroni/justdoit/MainActivity.java
@@ -1,9 +1,11 @@
 package ca.cerroni.justdoit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +17,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main_activity, menu);
         return true;
 
+    }
+
+    public void manageScreen(MenuItem item) {
+        Intent intent = new Intent(this, ManageTask.class);
+        startActivity(intent);
     }
 }
