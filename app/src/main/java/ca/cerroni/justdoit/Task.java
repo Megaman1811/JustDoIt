dc97c9df3c84ce828a404dd9c6c5abffadeff986
Zennii <contact@zenny3d.com>
2018-12-02 20:19:04 -0500

    Parcelable task

--- app/src/main/java/ca/cerroni/justdoit/Task.java
+++ app/src/main/java/ca/cerroni/justdoit/Task.java
@@ -1,8 +1,11 @@
 package ca.cerroni.justdoit;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.sql.Date;
 
-public class Task {
+public class Task implements Parcelable {
     public int id;
     public String name;
     public String notes;
@@ -14,4 +17,43 @@ public class Task {
     public boolean done;
     public Date snooze;
     public Date claimed;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+        dest.writeString(notes);
+        dest.writeString(startDate.toString());
+        dest.writeString(endDate.toString());
+        dest.writeString(time);
+        dest.writeInt(freq);
+        dest.writeString(color);
+    }
+
+    public static final Parcelable.Creator<Task> CREATOR = new Parcelable.Creator<Task>(){
+
+        @Override
+        public Task createFromParcel(Parcel s) {
+            Task t = new Task();
+            t.id = s.readInt();
+            t.name = s.readString();
+            t.notes = s.readString();
+            t.startDate = Date.valueOf(s.readString());
+            t.endDate = Date.valueOf(s.readString());
+            t.time = s.readString();
+            t.freq = s.readInt();
+            t.color = s.readString();
+            return t;
+        }
+
+        @Override
+        public Task[] newArray(int size) {
+            return new Task[size];
+        }
+    };
 }
